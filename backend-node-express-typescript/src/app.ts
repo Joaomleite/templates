6cdc51a08436c7ba21d7f4bf3666023ddf080835
Lucas Henrique <lucas.araujo@uds.com.br>
2022-05-26 10:35:15 -0300

    add: change backend templete

--- backend-node-express-typescript/src/app.ts
+++ backend-node-express-typescript/src/app.ts
@@ -1,10 +1,10 @@
-import Server from "./server";
+import Server from './server';
 
-process.env.TZ = "America/Sao_Paulo";
+process.env.TZ = 'America/Sao_Paulo';
 
 const server = new Server();
 
-function normalizePort(val: string, defaultPort: number): number {
+function normalizePort (val: string, defaultPort: number): number {
   const port = Number.parseInt(val, 10);
   if (Number.isNaN(port)) {
     return defaultPort;
@@ -15,16 +15,18 @@ function normalizePort(val: string, defaultPort: number): number {
   return defaultPort;
 }
 
-function onError(error: any): void {
-  if (error.syscall !== "listen") {
+const port = normalizePort(process.env.PORT || '', 8000);
+
+function onError (error: any): void {
+  if (error.syscall !== 'listen') {
     throw error;
   }
-  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
   switch (error.code) {
-    case "EACCES":
-      console.error(`${bind} requires elevated privileges`); 
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
       process.exit(1);
-    case "EADDRINUSE":
+    case 'EADDRINUSE':
       console.error(`${bind} is already in use`);
       process.exit(1);
     default:
@@ -32,8 +34,6 @@ function onError(error: any): void {
   }
 }
 
-const port = normalizePort(process.env.PORT || "", 8000);
-
 server.startHttp(port); // Ambiente de Desenvolvimento
 
-server.express.on("error", onError);
+server.express.on('error', onError);
